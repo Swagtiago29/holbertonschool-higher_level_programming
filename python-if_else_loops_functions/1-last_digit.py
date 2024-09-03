d71cd62178a54d514ce4782eeb3d9897c6cd4091
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-03 21:02:15 +0000

    task 1 done

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -7,14 +7,14 @@ str3 = "and is less than 6 and not 0"
 ld = abs(number) % 10
 
 if number < 0:
-    if ld == 0: 
+    if ld == 0:
         print(f"{str1} {number} is -{ld} and is zero")
     else:
         print(f"{str1} {number} is -{ld} {str3}")
 else:
     if ld > 5:
         print(f"{str1} {number} is {ld} {str2}")
-    elif ld == 0: 
+    elif ld == 0:
         print(f"{str1} {number} is {ld} and is 0")
     else:
         print(f"{str1} {number} is {ld} {str3}")
