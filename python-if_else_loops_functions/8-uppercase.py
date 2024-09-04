c583e64ba30f65f577dadd4de4f3fad505ca22d4
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-04 14:39:36 +0000

    8 fix fix fix fix fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -6,6 +6,6 @@ def uppercase(str):
     for i in range (0, x, 1):
         o = ord(str[i]) 
         if ord(str[i]) > 96 and ord(str[i]) < 123:
-            o - 32
+            o = o - 32
         print("{}".format(chr(o)), end= '')
     print()
