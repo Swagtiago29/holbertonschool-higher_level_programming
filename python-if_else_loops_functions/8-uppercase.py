d7aaffbd1ef9b21ec29283502dea9f760e614956
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-04 14:36:53 +0000

    8 fix fix fix

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,7 +3,7 @@ def uppercase(str):
 
     i = 0
     x = len(str)
-    for str[i] in range (x, 1):
+    for str[i] in range (0, x, 1):
         o = ord(str[i]) 
         if ord(str[i]) > 96 and ord(str[i]) < 123:
             o - 32
