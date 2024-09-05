75e0b2cc87dcf595be0f3f90d50ba1d42dc4a394
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-05 19:16:20 +0000

    2 fix args

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 from sys import argv
 if __name__ == "__main__":
-    if len(argv) == 0:
+    if len(argv) == 1:
         print("0 arguments.")
-    elif len(argv) == 1:
+    elif len(argv) == 2:
         print("1 argument:")
     else:
-        print("{} arguments:".format(len(argv)))
-        for argv in range(len(argv)):
-            print("{}: {}".format(len(argv), argv[0]))
+        print("{} arguments:".format(len(argv) - 1))
+        for i in range(1, len(argv)):
+            print("{}: {}".format(i, argv[i]))
