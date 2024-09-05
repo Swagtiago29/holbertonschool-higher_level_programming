a8cdaceb5e746ab8ab383d68869348b65c01af64
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-05 01:45:23 +0000

    task 6 done done

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-for i in range (10):
-    for u in range (i + 1, 10):
-        print ("{}{}".format(i, u), end=', ')
+for i in range(10):
+    for u in range(i + 1, 10):
+        if i + u == 17:
+            print("{}{}".format(i, u))
+        else:
+            print("{}{}".format(i, u), end=', ')
