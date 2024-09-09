517005951efffb5788cf9f054a6a49998b9a9fdd
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-09 20:00:15 +0000

    5 done?

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    x = len(my_string)
-    for i in range(x):
-        if my_string[i] == 'C' or my_string[i] == 'c':
-            if i == 0:
-                str = my_string[i + 1:]
-            else:
-                str = my_string[: i] + my_string[i + 1:]
-    return str
+    str = ''
+    for char in my_string:
+        if char not in 'Cc':
+            str += char
+        else:
+            pass
+    return(str)
