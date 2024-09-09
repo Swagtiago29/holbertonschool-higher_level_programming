29bdfc3c6441758d8400ea317ea2bffc8c26ef65
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-09 18:49:58 +0000

    task 2 done

---
+++ python-data_structures/2-replace_in_list.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    ls = len(my_list)
+    if idx < 0 or idx > ls:
+        return my_list
+    else:
+        my_list[idx] = element
+        return my_list
