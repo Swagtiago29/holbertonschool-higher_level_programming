41e456c7c37105852110739ee382b6637077861a
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-10 15:53:44 +0000

    9 done!!!!!!

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    
-    if len(my_list) != 0:
-        for i in range(1, len(my_list)):
-            x = my_list[i - 1]
-            if my_list[i] > x:
-                x = my_list[i]
-            else:
-                pass
-    else:
+    if len(my_list) == 0:
         return None
+    x = my_list[0]
+    for i in my_list:
+        if i > x:
+            x = i
     return(x)
