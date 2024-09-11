cd85860e8b79d7f266d0254fa92e573e2c97adc7
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-11 22:44:29 +0000

    task 0 pycodestyle

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -2,6 +2,6 @@
 def square_matrix_simple(matrix=[]):
     niu_matrix = [i[:] for i in matrix]
     for row in range(len(matrix)):
-        for f in  range(len(matrix[row])):
-          niu_matrix[row][f] = matrix[row][f] * matrix[row][f]
+        for f in range(len(matrix[row])):
+            niu_matrix[row][f] = matrix[row][f] * matrix[row][f]
     return niu_matrix
