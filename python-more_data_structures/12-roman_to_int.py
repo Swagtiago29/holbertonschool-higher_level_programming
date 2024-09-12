e687949ea3f8f1a6262898564f2a5d3af96f2888
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-12 00:48:30 +0000

    task 12 done, pycodestyle fixed!

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 def checks(romano):
-    if 'I' == romano: return 1
-    if 'X' == romano: return 10
-    if 'C' == romano: return 100
-    if 'M' == romano: return 1000
-    if 'V' == romano: return 5
-    if 'L' == romano: return 50
-    if 'D' == romano: return 500
+    if 'I' == romano:
+        return 1
+    if 'X' == romano:
+        return 10
+    if 'C' == romano:
+        return 100
+    if 'M' == romano:
+        return 1000
+    if 'V' == romano:
+        return 5
+    if 'L' == romano:
+        return 50
+    if 'D' == romano:
+        return 500
+
+
 def roman_to_int(roman_string):
     if roman_string and isinstance(roman_string, str):
         x = 0
@@ -14,12 +23,12 @@ def roman_to_int(roman_string):
             if len(roman_string) == 1:
                 return checks(roman_string[i])
             elif len(roman_string) > i + 1:
-                    if checks(roman_string[i]) > checks(roman_string[i + 1]):
-                        x += checks(roman_string[i])
-                    elif checks(roman_string[i]) == checks(roman_string[i + 1]):
-                        x += checks(roman_string[i])
-                    else:
-                        x -= checks(roman_string[i])
+                if checks(roman_string[i]) > checks(roman_string[i + 1]):
+                    x += checks(roman_string[i])
+                elif checks(roman_string[i]) == checks(roman_string[i + 1]):
+                    x += checks(roman_string[i])
+                else:
+                    x -= checks(roman_string[i])
             elif len(roman_string) == i + 1:
                 roman_string[i] <= roman_string[i - 1]
                 x += checks(roman_string[i])
