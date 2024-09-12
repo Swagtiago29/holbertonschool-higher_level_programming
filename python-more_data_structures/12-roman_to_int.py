fbcc8c4e556ca4d63fbf47562c4f9ea8ab3c6daa
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-12 00:28:54 +0000

    added task 12

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+def checks(romano):
+    if 'I' == romano: return 1
+    if 'X' == romano: return 10
+    if 'C' == romano: return 100
+    if 'M' == romano: return 1000
+    if 'V' == romano: return 5
+    if 'L' == romano: return 50
+    if 'D' == romano: return 500
+def roman_to_int(roman_string):
+    x = 0
+    for i in range(len(roman_string)):
+        if len(roman_string) == 1:
+            return checks(roman_string[i])
+        elif len(roman_string) > i + 1:
+                if checks(roman_string[i]) > checks(roman_string[i + 1]):
+                    x += checks(roman_string[i])
+                elif checks(roman_string[i]) == checks(roman_string[i + 1]):
+                    x += checks(roman_string[i])
+                else:
+                    x -= checks(roman_string[i])
+        elif len(roman_string) == i + 1:
+            roman_string[i] <= roman_string[i - 1]
+            x += checks(roman_string[i])
+    return x
