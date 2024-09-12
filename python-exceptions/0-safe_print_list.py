78a5c052323a10918ce511cc2d9c9f95bfdf198c
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-12 18:22:22 +0000

    test 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,5 +8,5 @@ def safe_print_list(my_list=[], x=0):
             else:
                 print("{:d}".format(my_list[i]), end='')
     except IndexError:
-            print()
+            print(/n,end='')
             return i
