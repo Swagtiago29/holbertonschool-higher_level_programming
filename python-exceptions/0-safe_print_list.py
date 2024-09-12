98c6f9672b14f45c95bac51a21767349bd5858eb
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-12 18:56:20 +0000

    0 done

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -8,6 +8,7 @@ def safe_print_list(my_list=[], x=0):
                 count += 1
             else:
                 print("{:d}".format(my_list[i]), end='')
+                count += 1
     except IndexError:
         print()
         return count
