8289a260bc77631337e9cf75cec1e972c0518bf2
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-12 18:58:19 +0000

    backtrack 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    count = 0
-    try:
-        for i in range(x):
-            if i == x - 1:
-                print("{:d}".format(my_list[i]))
-                count += 1
-            else:
-                print("{:d}".format(my_list[i]), end='')
-                count += 1
-    except IndexError:
-        print()
-        return count
+    if x:
+        try:
+            for i in range(x):
+                if i == x - 1:
+                    print("{:d}".format(my_list[i]))
+                    return x
+                else:
+                    print("{:d}".format(my_list[i]), end='')
+            return 0
+        except IndexError:
+            print()
+            return i
