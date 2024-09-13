4f9db80e07f7947791e4ec46a9a2b5c016c8473d
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-13 19:55:00 +0000

    task 4

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
-    niu_list=[]
-    for i in range(list_length):
-        try:
-            result = my_list_1[i] / my_list_2[i]
-            niu_list.append(result)
-        except ZeroDivisionError:
-            print("division by 0")
-            niu_list.append(0)
-        except TypeError:
-            print("wrong type")
-            niu_list.append(0)
-        except IndexError:
-            print("ourt of range")
-            niu_list.append(0)
-    return niu_list
+    try:
+        niu_list=[]
+        for i in range(list_length):
+            try:
+                result = my_list_1[i] / my_list_2[i]
+                niu_list.append(result)
+            except ZeroDivisionError:
+                print("division by 0")
+                niu_list.append(0)
+            except TypeError:
+                print("wrong type")
+                niu_list.append(0)
+            except IndexError:
+                print("ourt of range")
+                niu_list.append(0)
+    finally:
+        return niu_list
