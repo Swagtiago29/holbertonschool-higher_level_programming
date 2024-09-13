09d1d2379666bb1441e392a49fef2cafacd8b326
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-13 19:01:38 +0000

    fix 3?

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,11 +2,11 @@
 def safe_print_division(a, b):
     try:
         c = a / b
-        return c
     except ZeroDivisionError:
-        return None
+        c = None
     finally:
         if c is None:
             print("Inside Result: None")
         else:
             print("Inside Result: {:.1f}".format(c))
+    return c
