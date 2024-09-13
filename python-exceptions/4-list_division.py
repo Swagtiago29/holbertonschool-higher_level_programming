82a97238a6da1719311689f14c24225ff8db49e2
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-13 19:56:43 +0000

    4 done?

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     try:
-        niu_list=[]
+        niu_list = []
         for i in range(list_length):
             try:
                 result = my_list_1[i] / my_list_2[i]
@@ -13,7 +13,7 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("wrong type")
                 niu_list.append(0)
             except IndexError:
-                print("ourt of range")
+                print("out of range")
                 niu_list.append(0)
     finally:
         return niu_list
