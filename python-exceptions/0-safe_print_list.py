d9547fac6a76fd6da2254f24323875f653102130
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-13 17:47:04 +0000

    added fixed 0, 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    if x:
-        try:
-            for i in range(x):
-                if i == x - 1:
-                    print("{:d}".format(my_list[i]))
-                    return x
-                else:
-                    print("{:d}".format(my_list[i]), end='')
-            return 0
-        except IndexError:
-            print()
-            return i
+    o = 0
+    try:
+        for i in range(x):
+                print("{:d}".format(my_list[i]), end='')
+                o+= 1
+    except IndexError:
+            pass
+    print()
+    return o
