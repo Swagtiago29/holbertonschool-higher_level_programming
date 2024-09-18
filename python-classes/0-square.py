1fd9a9ad40763f6d41f308442d4a1abfa577be30
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-18 12:51:46 +0000

    added 0

---
+++ python-classes/0-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+"""
+This module defines a Square class that represents a geometric square.
+"""
+
+
+class Square:
+    """
+    A class to represent a square.
+
+    Attributes:
+    ----------
+    side_length : float
+        The length of a side of the square.
+
+    Methods:
+    -------
+    area():
+        Returns the area of the square.
+
+    perimeter():
+        Returns the perimeter of the square.
+    """
