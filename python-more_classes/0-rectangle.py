13f78a024660ffe5611dfcd177969d3c7a644c10
Swagtiago29 <Sant29ramos@gmail.com>
2024-09-22 14:34:13 +0000

    doc 0

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,3 +1,17 @@
 #!/usr/bin/python3
+
+"""
+Module: rectangle_module
+
+This module provides a basic implementation of a Rectangle class.
+
+Classes:
+    Rectangule: A class representing a rectangle with basic properties.
+"""
+
 class Rectangule:
-    pass
+    """
+    Class: Rectangule
+
+    Represents a rectangle defined by its width and height.
+    """
