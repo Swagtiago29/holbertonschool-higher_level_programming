b372b481cdc1004f0a9f673cfa574081211fbebf
Swagtiago29 <sant29ramos@gmail.com>
2024-09-26 17:21:53 -0300

    10 please

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,6 +7,7 @@ a Square class that validates its size and calculates its area.
 
 Rec = __import__('9-rectangle').Rectangle
 
+
 class Square(Rec):
     """Represents a square, inheriting from the Rectangle class.
 
@@ -20,12 +21,13 @@ class Square(Rec):
         """Initializes a Square instance.
 
         Args:
-            size (int): The size of the square, which sets both width and height.
+            size (int): The size of the square, which sets both width and
+            height.
 
         Raises:
             ValueError: If size is not a positive integer.
             TypeError: If size is not an integer.
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)  # Call the Rectangle constructor
+        super().__init__(size, size)
         self.__size = size
