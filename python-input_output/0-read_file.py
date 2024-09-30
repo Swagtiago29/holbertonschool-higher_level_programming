a9c5998035dd9817f3cc53c93951c917871a2865
Swagtiago29 <sant29ramos@gmail.com>
2024-09-30 16:41:38 -0300

    fixed module doc 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/binpython3
 def read_file(filename=""):
     """
     Read and print the contents of a text file.
@@ -6,6 +6,9 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the file to read. Defaults to an empty string,
                         which may lead to an error if no filename is provided.
+
+    Example:
+        read_file("example.txt")
     """
     with open(filename, 'r') as f:
         rid = f.read()
