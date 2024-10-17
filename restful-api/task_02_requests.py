86ea8e4d291510e78a3a6738c99bb6a0b31f091e
Swagtiago29 <sant29ramos@gmail.com>
2024-10-17 16:40:09 -0300

    added 2 ' requests'

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+import requests
+import csv
+
+def fetch_and_print():
+      req = requests.get('https://jsonplaceholder.typicode.com/posts')
+      print(f"Status Code: {req.status_code}")
+      if req.status_code == 200:
+            resp = req.json()
+            for posts in resp:
+                  print(posts['title'])
+            
+#fetch_and_print()
+
+def fetch_and_save_posts():
+      req = requests.get('https://jsonplaceholder.typicode.com/posts')
+      if req.status_code == 200:
+            resp = req.json()
+      else:
+        print(f"Request failed with status: {req.status_code}")
