d4cc312ace4202a84d1267c7d779209278ed2329
Swagtiago29 <sant29ramos@gmail.com>
2024-10-17 17:13:19 -0300

    finish 02

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -10,11 +10,17 @@ def fetch_and_print_posts():
             for posts in resp:
                   print(posts['title'])
             
-#fetch_and_print_posts()
-
 def fetch_and_save_posts():
       req = requests.get('https://jsonplaceholder.typicode.com/posts')
       if req.status_code == 200:
             resp = req.json()
+            posts = []
+            for data in resp:
+                  cositas = {'id': data['id'], 'title': data['title'], 'body': data['body']}
+                  posts.append(cositas)
+            with open('posts.csv', mode='w', newline='', encoding='utf-8') as f:
+                  writer = csv.DictWriter(f,['id', 'title', 'body'])
+                  writer.writeheader()
+                  writer.writerows(posts)
       else:
-        print(f"Request failed with status: {req.status_code}")
+        print(f"Failed with status: {req.status_code}")
