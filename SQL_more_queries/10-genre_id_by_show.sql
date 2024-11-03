a47f8c5e3e455c0a44cc182487d0e0285aee0ba2
Swagtiago29 <sant29ramos@gmail.com>
2024-11-03 19:27:42 -0300

    10

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,4 +1,4 @@
 -- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
-SELECT title, tv_show_genres.genre_id FROM tvshows
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tvshows
 JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 ORDER BY title ASC, tv_show_genres.genre_id ASC;
