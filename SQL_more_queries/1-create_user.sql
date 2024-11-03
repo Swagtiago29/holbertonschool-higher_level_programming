ba3b4ddf565303825c3b2045263a0345ce79304d
Swagtiago29 <sant29ramos@gmail.com>
2024-11-03 12:15:13 -0300

    ADDED 0 AND 1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,4 @@
+-- create user and give privileges
+CREATE USER IF USER NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
+-- grants privileges to user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
