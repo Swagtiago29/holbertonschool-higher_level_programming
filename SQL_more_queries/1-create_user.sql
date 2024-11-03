0f8b5f55d99ce09d740fd4f6ba7008c77e667354
Swagtiago29 <sant29ramos@gmail.com>
2024-11-03 12:26:59 -0300

    fixed comments

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,4 +1,3 @@
 -- create user and give privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
--- grants privileges to user
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
