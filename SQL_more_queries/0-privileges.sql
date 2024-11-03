8680badcd7fad056cb70f8a036e4c3d05f343d3f
Swagtiago29 <sant29ramos@gmail.com>
2024-11-03 12:17:37 -0300

    fixed spacec between @

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- lists permits of user_0d_1
-SHOW GRANTS FOR 'user_0d_1' @ 'localhost'
+SHOW GRANTS FOR 'user_0d_1'@'localhost'
 
 -- lists permits of user_0d_2
-SHOW GRANTS FOR 'user_0d_2' @ 'localhost'
+SHOW GRANTS FOR 'user_0d_2'@'localhost'
